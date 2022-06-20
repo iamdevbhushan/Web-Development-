2dc42df62a9f67158902a7baaefd178f91374afc
iamdevbhushan <93434472+iamdevbhushan@users.noreply.github.com>
2022-06-20 10:27:21 +0530

    Rename app.js/Expresscode to app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// const express = require('express')
-// const app = express()
-// app.get('/' ,(req,res) => {
-//   res.send("Hey whatsup from express")
-// })
-//
-// app.listen(3000);
-const fs = require('fs');
-const fileName = 'target.txt';
-fs.watch(fileName, () => console.log('File changed'));
+// Express Server code .
+const express = require('express')
+ const app = express()
+ app.get('/' ,(req,res) => {
+  res.send("Hey whatsup from express")
+})
+ app.listen(3000);
+
