d76dde0fc23001c7093316b990e911d685f35ddb
iamdevbhushan <93434472+iamdevbhushan@users.noreply.github.com>
2022-06-20 10:36:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// const express = require('express')
-// const app = express()
-// app.get('/' ,(req,res) => {
-//   res.send("Hey whatsup from express")
-// })
-//
-// app.listen(3000);
-const fs = require('fs');
-const fileName = 'target.txt';
-fs.watch(fileName, () => console.log('File changed'));
+//Express Code 
+const express = require('express')
+ const app = express()
+app.get('/' ,(req,res) => {
+  res.send("Hey whatsup from express")
+})
+
+ app.listen(3000);
+
